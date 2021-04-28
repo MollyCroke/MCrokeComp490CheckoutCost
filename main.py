98c7bad5afae0a2a8091db93348fbaef20a53878
Molly <m1croke@student.bridgew.edu>
2021-04-27 22:36:12 -0400

    function takes in multiple items

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ Maine:
 Clothing*5.5
 WIC food none
 Everything else *5.5
-"""
+
 from dataclasses import dataclass
 
 
@@ -18,35 +18,54 @@ class item:
     object_name: str
     object_type: str
     object_cost: int
+"""
+
+
+class item:
+    def __init__(self, object_name, object_type, object_cost):
+        self.object_name = object_name
+        self.object_type = object_type
+        self.object_cost = object_cost
+
+
+grocery_list = []
+grocery_list.append(item('shoes', "clothing", 20))
+grocery_list.append(item('carrot', "WIC", 40))
+grocery_list.append(item('shirt', "clothing", 200))
 
 
 def calculate_checkout(state, items):
     amount = 0
-    if state == "Massachusetts":
-        if items.object_type == "clothing":
-            if items.object_cost > 175:
-                x = (items.object_cost * .0625) + items.object_cost
+    revenue = 0
+    for x in items:
+        if state == "Massachusetts":
+            if x.object_type == "clothing":
+                if x.object_cost > 175:
+                    j = (x.object_cost * .0625) + x.object_cost
+                else:
+                    j = x.object_cost
+            elif x.object_type == "WIC":
+                j = x.object_cost
             else:
-                x = items.object_cost
-        elif items.object_type == "WIC":
-            x = items.object_cost
+                j = (x.object_cost * .0625) + x.object_cost
+        elif state == "New Hampshire":
+            j = x.object_cost
+        elif state == "Maine":
+            if x.object_type == "clothing":
+                j = (x.object_cost * .055) + x.object_cost
+            elif x.object_type == "WIC":
+                j = x.object_cost
+            else:
+                j = (x.object_cost * .055) + x.object_cost
         else:
-            x = (items.object_cost * .0625) + items.object_cost
-    elif state == "New Hampshire":
-        x = items.object_cost
-    else:
-        if items.object_type == "clothing":
-            x = (items.object_cost * .055) + items.object_cost
-        elif items.object_type == "WIC":
-            x = items.object_cost
-        else:
-            x = (items.object_cost * .055) + items.object_cost
-    revenue = x
-    amount = revenue + amount
-    print(amount)
-    return amount
+            print("Incorrect state")
+            exit()
+        amount = j
+        revenue = revenue + amount
+    print(revenue)
+    return revenue
 
 
 if __name__ == '__main__':
     purchase = item("sneakers", "clothing", 200)
-    print(type(calculate_checkout("Massachusetts", purchase)))
+    calculate_checkout("Massachusetts", grocery_list)
--- tests/test_file.py
+++ tests/test_file.py
@@ -1,13 +1,25 @@
 import main
-from dataclasses import dataclass
+
+
+class item:
+    def __init__(self, object_name, object_type, object_cost):
+        self.object_name = object_name
+        self.object_type = object_type
+        self.object_cost = object_cost
 
 
 def test_checkout():
-    @dataclass
-    class item:
-        object_name: str
-        object_type: str
-        object_cost: int
+    grocery_list = []
+    grocery_list.append(item('shoes', "clothing", 20))
+    grocery_list.append(item('carrot', "WIC", 40))
+    grocery_list.append(item('shirt', "clothing", 200))
+    assert main.calculate_checkout("Massachusetts", grocery_list) == 272.5
 
-    purchases = item("sneakers", "clothing", 10)
-    assert main.calculate_checkout("Massachusetts", purchases) == 10
+
+def test_large_checkout():
+    grocery_list = []
+    grocery_list.append(item('shoes', "clothing", 45))
+    grocery_list.append(item('carrot', "WIC", 10))
+    grocery_list.append(item('shirt', "clothing", 200))
+    grocery_list.append(item("tv", "other", 600))
+    assert main.calculate_checkout("New Hampshire", grocery_list) == 855
